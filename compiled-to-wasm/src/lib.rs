e77b6a16f76c3915bde736b8c5403f3c7f809c7e
Wojciech Geisler <wojciech.geisler@gmail.com>
2022-02-03 00:24:13 +0200

    Implement WASM runner and an example of WASM module

--- compiled-to-wasm/src/lib.rs
+++ compiled-to-wasm/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// no_mangle preserves the name `is_leap_year`
+// pub extern "C" allows the function to be called from outside WASM
+#[no_mangle]
+pub extern "C" fn is_leap_year(year: i32) -> i32 {
+    // Logically, this function returns a boolean value.
+    // However, the binary WebAssembly  format supports only integer types.
+    // To avoid inconsistency when we move to invoking this function from Rust,
+    // I'm using i32 explicitly here.
+
+    if year % 4 == 0 && (year % 100 != 0 || (year % 400 == 0)) {
+        return 1;
+    } else {
+        return 0;
     }
 }
--- wasm-runner/src/main.rs
+++ wasm-runner/src/main.rs
@@ -1,3 +1,48 @@
+use std::io::Read;
+use wasmi::{ImportsBuilder, ModuleInstance, NopExternals, RuntimeValue};
+
+fn print_usage() {
+    println!("Usage: wasm-runner path/to/file.wasm");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // read path to the .wasm file from arguments
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        print_usage();
+        return;
+    }
+    let wasm_file_path = std::path::Path::new(&args[1]);
+
+    // open and read the file with compiled WebAssembly
+    let mut wasm_binary = vec![];
+    std::fs::File::open(wasm_file_path)
+        .expect("failed to open WASM file")
+        .read_to_end(&mut wasm_binary)
+        .expect("failed to read WASM file");
+
+    // deserialize and instantiate a WebAssembly module using wasmi
+    let module = wasmi::Module::from_buffer(&wasm_binary).expect("failed to load wasm");
+    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+        .expect("failed to instantiate wasm module")
+        .assert_no_start();
+
+    // invoke a function from the WebAssembly module and verify the expected return value
+    let year_to_check = 2022;
+    let return_value = instance
+        .invoke_export(
+            "is_leap_year",
+            &[RuntimeValue::I32(year_to_check)],
+            &mut NopExternals,
+        )
+        .expect("failed to execute export");
+
+    println!("Return value: {:?}", return_value);
+
+    match return_value {
+        Some(RuntimeValue::I32(1)) => println!("Year {} is a leap year", year_to_check),
+        Some(RuntimeValue::I32(0)) => println!("Year {} is not a leap year", year_to_check),
+        Some(_) => panic!("is_leap_year returned a value of unexpected type"),
+        None => panic!("is_leap_year did not return a value"),
+    };
 }
